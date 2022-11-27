c0dd1f52f10e8bea80e148732078d81d55725c58
Brandy Posthuma <blposthuma@yahoo.com>
2022-11-27 16:26:11 -0700

    finished adding code to generate password

--- script.js
+++ script.js
@@ -1,91 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var UppercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; //26 letters
+var LowercaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //26 letters
+var Specialchar = ["$", "~", ",", "{", "}", "`", "^", "!", ", ", "#", "%", "@", "&", "'", "*", "+", ".", "/", ":", "<", ">", "?", "=",]; //23 special characters used
+var Numberchar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; // 10 numbers including 0
+var Random = []
+var Temppass = []
 
-var passwordInfo = {
-
-  //how long is the password
-  passwordLength: 0,
-
-// lowercase characters
-passwordLowercaseChar: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-
-// UPPERCASE CHARACTERS
-passwordUppercaseChar: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-
-// Numbers
-passwordNumberchar: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-
-// Speciacl Characters
-passwordSpecialchar: ["$", "~", ",", "{", "}", "`", "^", "!", ", ", "#", "%", "@", "&", "'", "*", "+", ".", "/", ":", "<", ">", "?", "=",], 
-}
-
-
-// Write password to the #password input (given code for the assignment)
+// Write password to the #password input (given code for the assignment and it will display the password on the page once it is generated)
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-  };
-
-// Add event listener to generate button (given code for the assignment)
-generateBtn.addEventListener("click", writePassword);
-
-function generatePassword() {
-  var result = "";
-
-  //Different types of Variables to recieve from prompts from the user
-  var passwordLength = 0;
-  var passwordUppercaseChar;
-  var passwordLowercaseChar;
-  var passwordNumberchar;
-  var passwordSpecialchar;
-
-  //Information to Initilize password
-  passwordLength = 0;
-  passwordInfo.passwordLength = 0;
-  result = "";
-
-  while (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("How many characters do you want your password to be? \nPassword has to be between 8 and 128 characters in length."):
-
-    if (passwordLength === null) {
-      return "Your secure password";
-    }
-    else {
-
-      if (!isFinite(passwordLength)) {
-        alert("Please enter a number");
-        return "Your secure password";
-      }
-      else {
-        //this is to check the length of the password and meets the lenght requirements
-        if (passwordLength < 8 || passwordLength > 128) {
-          alert("The password must be between 8 and 128 characters in length.");
-          return "your secure password";
-        }
-        else {
-
-          showPrompts();
-
-          while (passwordInfo.passwordLength < passwordLength) {
-            if (passwordLowercaseChar === false && passwordUppercaseChar === false && passwordNumberchar === false && passwordSpecialchar === false) {
-              alert("You must select at least one criteria of lowercase, uppercase, numbers or special chatacters")
-              showPrompts();
-            }
-            else {
-              if (passwordLowercaseChar === true && passwordInfo.passwordLength < passwordLength) {
-                var lowercase = passwordInfo.passwordLowercaseChar[Math.floor(Math.random()*26)]
-                result = result + lowercase;
-                passwordInfo.passwordLength++;
-              }
-              if 
-            }
-          }
-
-
-        }
-      }
-    }
+  var passwordLength = window.prompt("Pick a password length between 8-128 characters.")
+  if (passwordLength >=8 || passwordLength <= 128){
   }
 
-}
+//Different types of Variables to recieve from prompts from the user
+  if (confirm("Would you like to use uppercase letters?")==true) {
+    Random = Random.concat(UppercaseChar);
+  } else {
+    text = "No issues.";
+  }
+  if (confirm("Would you like to use lowercase letters?")==true) {
+    Random = Random.concat(LowercaseChar);
+  } else {
+    text = "No issues.";
+  }  
+  if (confirm("Would you like to use any special characters?") ==true) {
+    Random = Random.concat(Specialchar);
+  } else {
+    text = "No issues.";
+  }
+  if (confirm("Would you like to use any numbers?") ==true) {
+    Random = Random.concat(Specialchar);
+  } else {
+    text = "No issues.";
+  };
+  console.log(Random);
+// Using a While Loop to repeat the code until the specific user inputs are met 
+//the code will continue to push to temp array with the random criteria
+  while (Temppass.length != passwordLength) {
+    Temppass.push(generatePassword(Random));
+  }
+  console.log(Temppass)
+//create temp password
+  var passwordText = document.querySelector("#password");
+  passwordText.value = Temppass.join(''); //using joing here to remove commas between
+} 
+function generatePassword(array){
+  return array[Math.floor(Math.random() * array.length)];
+} 
+//Event listener - click on generate button
+generateBtn.addEventListener("click", writePassword);
